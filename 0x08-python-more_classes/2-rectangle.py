daa6bd47c8482786d43bd72d1e7fbc601f2da15e
thugginnn <paul.iyiade4@gmail.com>
2023-10-30 19:54:15 -0700

    2-rectangle.py

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+""" empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """ Rectangle class with private attributes width and height."""
+    def __init__(self, width=0, height=0):
+        """ Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the rectangle. Default is 0.
+            height (int): The height of the rectangle. Default is 0.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Get the width of the rectangle.
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """ Get the height of the rectangle.
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """ Set the width of the rectangle.
+
+        Args:
+            value (int): The width to set.
+
+        Raises:
+            TypeError: If width is not an integer.
+            ValueError: If width is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """ Set the height of the rectangle.
+
+        Args:
+            value (int): The height to set.
+
+        Raises:
+            TypeError: If height is not an integer.
+            ValueError: If height is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Calculate and return the area of rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Calculate and return the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
