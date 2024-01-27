e0a78226f9641b26b29c9d61acb41faf8174fe72
thugginnn <paul.iyiade4@gmail.com>
2024-01-26 19:33:13 -0800

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,13 @@
-#!/usr/bin/pytohn3
-"""Displays the value of X-Request-Id in the response header"""
+#!/usr/bin/python3
+"""Displays the value of X-Request-Id in the response
+header"""
 
 import requests
 import sys
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
-    response = requests.get(url)
-    if 'X-Request-Id' in response.headers:
-        x_request_id = response.headers['X-Request-Id']
-        print(x_request_id)
+
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
