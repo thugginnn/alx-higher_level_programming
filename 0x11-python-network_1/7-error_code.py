5c87269f5847c65e55e61b86cb567a0c183e1d56
thugginnn <paul.iyiade4@gmail.com>
2024-01-26 19:36:54 -0800

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Displays the body of the response or handles HTTPError"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
